0b82af2487e4600d7db4f46d0a0c05ddc4c205e8
nuttyCream <issanutty@gmail.com>
2025-03-22 23:05:56 -0700

    build: rm bindgen from build script

--- build.rs
+++ build.rs
@@ -1,57 +1,63 @@
 use std::path::PathBuf;
-use std::{env, panic};
+use std::{env, process::Command};
 
+// no longer depends on bindgen
+// will write bindings on my own
 fn main() {
     println!("cargo:rerun-if-changed=hardware/gpio.h");
+    println!("cargo:rerun-if-changed=hardware/gpio.c");
 
     let libdir_path = PathBuf::from("hardware")
         .canonicalize()
         .expect("cannot canonicalize path");
 
-    let headers_path = libdir_path.join("gpio.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
+    let target = env::var("TARGET").unwrap_or_else(|_| String::from("x86_64-unknown-linux-gnu"));
+    println!("cargo:warning=Building for target: {}", target);
+
+    let compiler = if target == "aarch64-unknown-linux-musl" {
+        env::var("CC_AARCH64_UNKNOWN_LINUX_MUSL")
+            .unwrap_or_else(|_| String::from("aarch64-unknown-linux-musl-gcc"))
+    } else {
+        String::from("clang")
+    };
+    println!("cargo:warning=Using compiler: {}", compiler);
 
     let obj_path = libdir_path.join("gpio.o");
     let lib_path = libdir_path.join("libgpio.a");
 
-    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
-
-    println!("cargo:rustc-link-lib=gpio");
-
-    if !std::process::Command::new("clang")
+    let status = Command::new(&compiler)
+        .current_dir(&libdir_path)
         .arg("-c")
         .arg("-o")
         .arg(&obj_path)
-        .arg(libdir_path.join("gpio.c"))
-        .output()
-        .expect("could not spawn 'clang'")
-        .status
-        .success()
-    {
-        panic!("could not compile object file");
+        .arg("gpio.c")
+        .status()
+        .expect(&format!("Failed to execute {}", compiler));
+
+    if !status.success() {
+        panic!("Failed to compile gpio.c");
     }
 
-    if !std::process::Command::new("ar")
+    let archiver = if target == "aarch64-unknown-linux-musl" {
+        env::var("AR_AARCH64_UNKNOWN_LINUX_MUSL")
+            .unwrap_or_else(|_| String::from("aarch64-unknown-linux-musl-ar"))
+    } else {
+        String::from("ar")
+    };
+    println!("cargo:warning=archiver: {}", archiver);
+
+    let status = Command::new(&archiver)
+        .current_dir(&libdir_path)
         .arg("rcs")
         .arg(lib_path)
         .arg(obj_path)
-        .output()
-        .expect("could not spawn 'ar'")
-        .status
-        .success()
-    {
-        panic!("could not emit library path");
+        .status()
+        .expect(&format!("Failed to execute {}", archiver));
+
+    if !status.success() {
+        panic!("Failed to create static library");
     }
 
-    let bindings = bindgen::Builder::default()
-        .header(headers_path_str)
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("unable to generate bindings");
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
-
-    bindings
-        .write_to_file(out_path)
-        .expect("couldnt write bindings");
+    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
+    println!("cargo:rustc-link-lib=gpio");
 }
