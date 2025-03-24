37aaf8178c4f987a5d242e6776eb2207c21a94cf
nuttyCream <issanutty@gmail.com>
2025-03-23 19:54:13 -0700

    feat: add webby skelly jelly smelly

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-//use axum::{routing::get, Router};
 mod bindings;
+mod server;
+
 use bindings::Gpio;
 
-use std::{thread, time::Duration};
+use axum::{
+    http::header,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use std::{
+    fs,
+    path::{self, Path},
+    thread,
+    time::Duration,
+};
 
-fn main() -> Result<(), &'static str> {
-    println!("starting");
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(serve_html))
+        .route("/htmx.min.js", get(serve_js))
+        .route("/style.css", get(serve_css));
 
-    let gpio = Gpio::new(17)?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
 
-    gpio.set_as_output()?;
+async fn serve_html() -> Html<&'static str> {
+    let html = include_str!("../frontend/index.html");
+    Html(html)
+}
 
-    for _i in 0..5 {
-        println!("on");
-        gpio.set_high()?;
-        thread::sleep(Duration::from_millis(500));
+async fn serve_js() -> impl IntoResponse {
+    let js = include_str!("../frontend/htmx.min.js");
+    ([(header::CONTENT_TYPE, "application/javascript")], js)
+}
 
-        println!("off");
-        gpio.set_low()?;
-        thread::sleep(Duration::from_millis(500));
-    }
-
-    println!("done");
-    Ok(())
+async fn serve_css() -> impl IntoResponse {
+    let css = include_str!("../frontend/style.css");
+    ([(header::CONTENT_TYPE, "text/css")], css)
 }
