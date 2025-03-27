a592ce6259fa9b69061c814f3d3170364fe97513
nuttyCream <issanutty@gmail.com>
2025-03-27 02:16:36 -0700

    feat: example websocket impl

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,98 @@
 mod bindings;
-
-use bindings::Gpio;
+mod errors;
 
 use axum::{
     http::header,
-    response::{Html, IntoResponse},
-    routing::get,
+    response::{
+        Html,
+        IntoResponse,
+    },
+    routing::{
+        any,
+        get,
+    },
     Router,
 };
-use std::{
-    fs,
-    path::{self, Path},
-    thread,
-    time::Duration,
+use bindings::{
+    Gpio,
+    GpioController,
 };
+use fastwebsockets::{
+    upgrade,
+    OpCode,
+    WebSocketError,
+};
+use listenfd::ListenFd;
+use std::{
+    env,
+    error::Error,
+    net::SocketAddr,
+};
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
+    let port = env::args()
+        .nth(1)
+        .and_then(|arg| arg.parse::<i32>().ok())
+        .unwrap_or(3000);
+    let addr = format!("0.0.0.0:{port}");
+
     let app = Router::new()
         .route("/", get(serve_html))
         .route("/htmx.min.js", get(serve_js))
-        .route("/style.css", get(serve_css));
+        .route("/style.css", get(serve_css))
+        .route("/ws", any(ws_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => {
+            listener.set_nonblocking(true)?;
+            TcpListener::from_std(listener)?
+        }
+        None => TcpListener::bind(addr).await?,
+    };
+
+    Gpio::new()?;
+
     println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
+
+    Ok(())
+}
+
+async fn ws_handler(ws: upgrade::IncomingUpgrade) -> impl IntoResponse {
+    let (response, fut) = ws.upgrade().unwrap();
+
+    tokio::task::spawn(async move {
+        if let Err(e) = handle_client(fut).await {
+            eprintln!("Error in websocket connection: {}", e);
+        }
+    });
+
+    response
+}
+
+async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
+    let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
+
+    loop {
+        let frame = ws.read_frame().await?;
+        match frame.opcode {
+            OpCode::Close => break,
+            OpCode::Text | OpCode::Binary => {
+                ws.write_frame(frame).await?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
 
 async fn serve_html() -> Html<&'static str> {
