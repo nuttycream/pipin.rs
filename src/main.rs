27b3f086c5680a8765d1995455e1c924eff33452
nuttyCream <issanutty@gmail.com>
2025-03-28 19:55:20 -0700

    feat: add toggling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod bindings;
 mod errors;
+mod logger;
 
 use axum::{
+    extract::{
+        Path,
+        State,
+    },
     http::header,
     response::{
         Html,
@@ -20,10 +25,15 @@ use fastwebsockets::{
     WebSocketError,
 };
 use listenfd::ListenFd;
+use logger::LogType;
 use std::{
     env,
     error::Error,
     net::SocketAddr,
+    sync::{
+        Arc,
+        Mutex,
+    },
     time::{
         SystemTime,
         UNIX_EPOCH,
@@ -39,12 +49,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap_or(3000);
     let addr = format!("0.0.0.0:{port}");
 
+    let gpio = Arc::new(Mutex::new(Gpio::new()));
+
     let app = Router::new()
         .route("/", get(serve_html))
         .route("/htmx.min.js", get(serve_js))
         .route("/style.css", get(serve_css))
         .route("/ws", get(ws_handler))
-        .route("/setup", get(setup));
+        .route("/setup", get(setup))
+        .route("/{pin}", get(toggle))
+        .with_state(gpio);
 
     let mut listenfd = ListenFd::from_env();
     let listener = match listenfd.take_tcp_listener(0)? {
@@ -55,25 +69,82 @@ async fn main() -> Result<(), Box<dyn Error>> {
         None => TcpListener::bind(addr).await?,
     };
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    println!("listening on {}", listener.local_addr()?);
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
-    .await
-    .unwrap();
+    .await?;
 
     Ok(())
 }
 
-async fn setup() -> impl IntoResponse {
-    // handle this error
-    // honestly we should restructure this
-    // instead of creating a new Gpio context
-    // we should have main handle the reference count
-    // likely with arc
-    // then setup is a separate function (?)
-    let _gpio = Gpio::new();
+async fn toggle(
+    State(gpio): State<Arc<Mutex<Gpio>>>,
+    Path(pin): Path<String>,
+) -> impl IntoResponse {
+    let mut gpio = gpio.lock().unwrap();
+
+    // refactor this out bum
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default()
+        .as_secs();
+    let hours = (now / 3600) % 24;
+    let minutes = (now / 60) % 60;
+    let seconds = now % 60;
+
+    if !gpio.is_initialized() {
+        return Html(format!(
+            r#"<div class="log-entry">
+                    <span class="log-time">[{:02}:{:02}:{:02}]</span>
+                    <span class="log-error">gpio not initialized/setup</span>
+                </div>"#,
+            hours, minutes, seconds
+        ));
+    }
+
+    let gpio_pin = match pin.parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => {
+            return Html(format!(
+                r#"<div class="log-entry">
+                    <span class="log-time">[{:02}:{:02}:{:02}]</span>
+                    <span class="log-error">invalid GPIO pin {}</span>
+                </div>"#,
+                hours, minutes, seconds, pin
+            ));
+        }
+    };
+
+    let res: String = match gpio.toggle(gpio_pin) {
+        Ok(_) => format!("toggled gpio {}", gpio_pin),
+        Err(e) => {
+            println!("{e}");
+            "failed to toggle gpio".to_string()
+        }
+    };
+
+    let log_entry = format!(
+        r#"<div class="log-entry">
+            <span class="log-time">[{:02}:{:02}:{:02}]</span>
+            <span class="log-info">{}</span>
+        </div>"#,
+        hours, minutes, seconds, res
+    );
+
+    Html(log_entry)
+}
+
+async fn setup(State(gpio): State<Arc<Mutex<Gpio>>>) -> impl IntoResponse {
+    let mut gpio = gpio.lock().unwrap();
+    let res = match gpio.setup() {
+        Ok(_) => "GPIO initialized",
+        Err(e) => {
+            println!("{e}");
+            "Failed to initialize gpio"
+        }
+    };
 
     // extract this to a helper func later on
     let now = SystemTime::now()
@@ -87,9 +158,9 @@ async fn setup() -> impl IntoResponse {
     let log_entry = format!(
         r#"<div class="log-entry">
             <span class="log-time">[{:02}:{:02}:{:02}]</span>
-            <span class="log-info">GPIO initialized</span>
+            <span class="log-info">{}</span>
         </div>"#,
-        hours, minutes, seconds
+        hours, minutes, seconds, res
     );
 
     Html(log_entry)
