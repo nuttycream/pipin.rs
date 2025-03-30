d34c2fb2e2b1dbcc8d8875d676fc3f6d003fac8b
nuttyCream <issanutty@gmail.com>
2025-03-29 17:31:03 -0700

    feat: add sighandler + appstate gpio shared mutex

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use fastwebsockets::{
     WebSocketError,
 };
 use listenfd::ListenFd;
-use logger::LogType;
+//use logger::LogType;
 use std::{
     env,
     error::Error,
@@ -41,6 +41,11 @@ use std::{
 };
 use tokio::net::TcpListener;
 
+#[derive(Clone)]
+struct AppState {
+    gpio: Arc<Mutex<Gpio>>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let port = env::args()
@@ -49,7 +54,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap_or(3000);
     let addr = format!("0.0.0.0:{port}");
 
-    let gpio = Arc::new(Mutex::new(Gpio::new()));
+    let appstate = AppState {
+        gpio: Arc::new(Mutex::new(Gpio::new())),
+    };
 
     let app = Router::new()
         .route("/", get(serve_html))
@@ -60,7 +67,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/reset", get(reset))
         .route("/terminate", get(terminate))
         .route("/{pin}", get(toggle))
-        .with_state(gpio);
+        .with_state(appstate);
 
     let mut listenfd = ListenFd::from_env();
     let listener = match listenfd.take_tcp_listener(0)? {
@@ -71,21 +78,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
         None => TcpListener::bind(addr).await?,
     };
 
+    let shutdown = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to get ctrl+c signhandle");
+    };
+
     println!("listening on {}", listener.local_addr()?);
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
+    .with_graceful_shutdown(shutdown)
     .await?;
 
     Ok(())
 }
 
-async fn toggle(
-    State(gpio): State<Arc<Mutex<Gpio>>>,
-    Path(pin): Path<String>,
-) -> impl IntoResponse {
-    let mut gpio = gpio.lock().unwrap();
+async fn toggle(State(appstate): State<AppState>, Path(pin): Path<String>) -> impl IntoResponse {
+    let mut gpio = appstate.gpio.lock().unwrap();
 
     // refactor this out bum
     let now = SystemTime::now()
@@ -95,16 +106,6 @@ async fn toggle(
     let hours = (now / 3600) % 24;
     let minutes = (now / 60) % 60;
     let seconds = now % 60;
-
-    if !gpio.is_initialized() {
-        return Html(format!(
-            r#"<div class="log-entry">
-                    <span class="log-time">[{:02}:{:02}:{:02}]</span>
-                    <span class="log-error">gpio not initialized/setup</span>
-                </div>"#,
-            hours, minutes, seconds
-        ));
-    }
 
     let gpio_pin = match pin.parse::<i32>() {
         Ok(num) => num,
@@ -123,7 +124,7 @@ async fn toggle(
         Ok(_) => format!("toggled gpio {}", gpio_pin),
         Err(e) => {
             println!("{e}");
-            "failed to toggle gpio".to_string()
+            format!("failed to toggle gpio {e}").to_string()
         }
     };
 
@@ -138,43 +139,13 @@ async fn toggle(
     Html(log_entry)
 }
 
-async fn setup(State(gpio): State<Arc<Mutex<Gpio>>>) -> impl IntoResponse {
-    let mut gpio = gpio.lock().unwrap();
-    let res = match gpio.setup() {
-        Ok(_) => "GPIO initialized",
+async fn setup(State(appstate): State<AppState>) -> impl IntoResponse {
+    let mut gpio = appstate.gpio.lock().unwrap();
+    let res: String = match gpio.setup() {
+        Ok(_) => "GPIO initialized".to_string(),
         Err(e) => {
             println!("{e}");
-            "Failed to initialize gpio"
-        }
-    };
-
-    // extract this to a helper func later on
-    let now = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap_or_default()
-        .as_secs();
-    let hours = (now / 3600) % 24;
-    let minutes = (now / 60) % 60;
-    let seconds = now % 60;
-
-    let log_entry = format!(
-        r#"<div class="log-entry">
-            <span class="log-time">[{:02}:{:02}:{:02}]</span>
-            <span class="log-info">{}</span>
-        </div>"#,
-        hours, minutes, seconds, res
-    );
-
-    Html(log_entry)
-}
-
-async fn reset(State(gpio): State<Arc<Mutex<Gpio>>>) -> impl IntoResponse {
-    let mut gpio = gpio.lock().unwrap();
-    let res = match gpio.reset() {
-        Ok(_) => "GPIO reset",
-        Err(e) => {
-            println!("{e}");
-            "Failed to reset gpio"
+            format!("failed to initialize gpio {e}")
         }
     };
 
@@ -198,13 +169,43 @@ async fn reset(State(gpio): State<Arc<Mutex<Gpio>>>) -> impl IntoResponse {
     Html(log_entry)
 }
 
-async fn terminate(State(gpio): State<Arc<Mutex<Gpio>>>) -> impl IntoResponse {
-    let mut gpio = gpio.lock().unwrap();
-    let res = match gpio.terminate() {
-        Ok(_) => "GPIO terminated",
+async fn reset(State(appstate): State<AppState>) -> impl IntoResponse {
+    let mut gpio = appstate.gpio.lock().unwrap();
+    let res: String = match gpio.reset() {
+        Ok(_) => "GPIO reset".to_string(),
         Err(e) => {
             println!("{e}");
-            "Failed to terminate gpio"
+            format!("failed to reset gpio {e}")
+        }
+    };
+
+    // extract this to a helper func later on
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap_or_default()
+        .as_secs();
+    let hours = (now / 3600) % 24;
+    let minutes = (now / 60) % 60;
+    let seconds = now % 60;
+
+    let log_entry = format!(
+        r#"<div class="log-entry">
+            <span class="log-time">[{:02}:{:02}:{:02}]</span>
+            <span class="log-info">{}</span>
+        </div>"#,
+        hours, minutes, seconds, res
+    );
+
+    Html(log_entry)
+}
+
+async fn terminate(State(appstate): State<AppState>) -> impl IntoResponse {
+    let mut gpio = appstate.gpio.lock().unwrap();
+    let res: String = match gpio.terminate() {
+        Ok(_) => "GPIO terminated".to_string(),
+        Err(e) => {
+            println!("{e}");
+            format!("failed to terminate gpio {e}")
         }
     };
 
