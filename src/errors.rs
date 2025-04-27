d1d7f2eb7264831c4b8ba2f1a7c660be6aa4857c
nuttyCream <issanutty@gmail.com>
2025-04-27 11:58:28 -0700

    chore: comment unused errors

--- src/errors.rs
+++ src/errors.rs
@@ -4,16 +4,18 @@ use std::{error::Error, fmt};
 pub enum GpioError {
     InvalidPin(i32),
     Setup,
+    NotInitialized,
+    /*
     Direction(i32),
     Set(i32),
     Terminate,
     SwitchDevice(i32),
-    NotInitialized,
     Clear(i32),
     PullDown(i32),
     PullUp(i32),
     HardwareDetection,
     InvalidDevice(i32),
+    */
 }
 
 impl Error for GpioError {}
@@ -24,39 +26,40 @@ impl fmt::Display for GpioError {
             GpioError::Setup => {
                 write!(f, "Failed to Initialize")
             }
-            GpioError::Direction(pin) => {
-                write!(f, "Failed to set direction {}", pin)
-            }
-            GpioError::Set(pin) => {
-                write!(f, "Failed to set high - low {}", pin)
-            }
-            GpioError::Terminate => {
-                write!(f, "Failed to terminate")
-            }
-            GpioError::SwitchDevice(dev) => {
-                write!(f, "Failed to switch device: {}", dev)
-            }
             GpioError::InvalidPin(pin) => {
                 write!(f, "Invalid gpio pin {}", pin)
             }
             GpioError::NotInitialized => {
                 write!(f, "GPIO Not Initialized")
-            }
-            GpioError::Clear(pin) => {
-                write!(f, "Failed to clear GPIO {}", pin)
-            }
-            GpioError::PullDown(pin) => {
-                write!(f, "Failed to set {} to pull down", pin)
-            }
-            GpioError::PullUp(pin) => {
-                write!(f, "Failed to set {} to pull up", pin)
-            }
-            GpioError::HardwareDetection => {
-                write!(f, "Failed to Detect Raspberry Pi Peripheral Address")
-            }
-            GpioError::InvalidDevice(dev) => {
-                write!(f, "Invalid device {}", dev)
-            }
+            } /*
+              GpioError::Direction(pin) => {
+                  write!(f, "Failed to set direction {}", pin)
+              }
+              GpioError::Set(pin) => {
+                  write!(f, "Failed to set high - low {}", pin)
+              }
+              GpioError::Terminate => {
+                  write!(f, "Failed to terminate")
+              }
+              GpioError::SwitchDevice(dev) => {
+                  write!(f, "Failed to switch device: {}", dev)
+              }
+              GpioError::Clear(pin) => {
+                  write!(f, "Failed to clear GPIO {}", pin)
+              }
+              GpioError::PullDown(pin) => {
+                  write!(f, "Failed to set {} to pull down", pin)
+              }
+              GpioError::PullUp(pin) => {
+                  write!(f, "Failed to set {} to pull up", pin)
+              }
+              GpioError::HardwareDetection => {
+                  write!(f, "Failed to Detect Raspberry Pi Peripheral Address")
+              }
+              GpioError::InvalidDevice(dev) => {
+                  write!(f, "Invalid device {}", dev)
+              }
+              */
         }
     }
 }
